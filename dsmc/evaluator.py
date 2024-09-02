ae82141ee2a8b020b5ecf6eb20a6a3ee586d28bc
Nicola J. Müller <106013851+nicola-mueller@users.noreply.github.com>
2024-09-02 16:38:46 +0200

    HOPEFULLY IT SAVES NOW

--- dsmc/evaluator.py
+++ dsmc/evaluator.py
@@ -1,7 +1,27 @@
 import numpy as np
 
-# base class for evaluation metrics
-class Metric:
+
+def CH(kappa, eps):
+    x = 1 / np.power(eps, 2)
+    y = np.log(2 / (kappa))
+    res = x * y
+
+    return int(np.floor(res))
+
+def APMC(s2, kappa, eps):
+    z = st.norm.ppf(1 - kappa / 2)
+
+    return np.ceil(4 * z * s2 / np.power(eps, 2))
+
+def get_variance(results, epsilon, kappa):
+    pass
+
+def construct_confidence_interval_length(results, epsilon, kappa):
+    pass
+
+
+# base class for evaluation properties
+class Property:
         def __init__(self, name):
             self.name = name
             pass
@@ -11,7 +31,7 @@ class Metric:
             pass
 
 # metric that checks if the goal has been reached
-class GoalReachingProbabilityMetric(Metric):
+class GoalReachingProbabilityProperty(Property):
     def __init__(self, name, goal_reward):
         super().__init__(name)
         self.goal_reward = goal_reward
@@ -23,7 +43,7 @@ class GoalReachingProbabilityMetric(Metric):
             return 0.0
 
 # metric that calculates the return of a trajectory
-class ReturnMetric(Metric):
+class ReturnProperty(Property):
     def __init__(self, name, gamma):
         super().__init__(name)
         self.gamma = gamma
@@ -34,84 +54,63 @@ class ReturnMetric(Metric):
             ret += trajectory[t][2] * np.power(self.gamma, t)
         return ret
 
-class Evaluator():
+class Evaluator:
 
     def __init__(self, env: GymEnv, gamma: float, initial_episodes: int, episodes_per_run: int, ):
         self.env = env
         self.gamma = gamma
-        self.initial_runs = initial_episodes
+        self.initial_episodes = initial_episodes
         self.episodes_per_run = episodes_per_run
-        self.metrics = {}
+        self.properties = {}
 
-    # register a new evaluation metric
-    def register_metric(self, metric: Metric):
-        self.metrics[metric.name] = metric
+    # register a new evaluation property
+    def register_metric(self, property: Property):
+        self.properties[property.name] = property
+
+    def run_policy(self, agent, num_episodes, results_per_property):
+        for _ in range(num_episodes):
+            state = self.env.reset()
+            trajectory = []
+            done = False
+            while not done:
+                action = agent.act(state)
+                next_state, reward, done, _ = self.env.step(action)
+                trajectory.append((state, action, reward))
+                state = next_state
+
+            for property in self.properties.values():
+                results_per_property[property.name].append(property.check(trajectory))
 
     def eval(self, agent, epsilon, kappa):
+        # initialize EvaluationResults object for each class and whether the property converged
+        results_per_property = {}
+        converged_per_property = {}
+        for property in self.properties.values():
+            results_per_property[property.name] = EvaluationResults(property=property.name)
+            converged_per_property[property.name] = False
 
+        # run initial episodes - one run, such that the first run of the while loop checks convergence
+        self.run_policy(agent, self.initial_episodes - self.episodes_per_run, results_per_property)
+        made_episodes = self.initial_episodes - self.episodes_per_run
 
-    def run_policy(self, n_episodes, l_episodes=100):
-        for i in range(1, n_episodes + 1):
-            obs, _ = self.env.reset()
-
-            ret = 0
-            for t in range(l_episodes):
-                action, _states = self.act_function(obs)
-                obs, r, done, truncated, _ = self.env.step(int(action))
-
-                ret += r * np.power(self.gamma, t)
-
-                if (done or truncated):
-                    break
-
-            self._update(ret)
-
-
-    # grps_eps: The Accuracy to be achieved for the ratio between wins and runs in general
-    # return_eps: The Accuracy to be achieved for the expected reward
-    # kappa: How sure we want to be we achieved accuracy the eps
-
-    # TODO: evaluate arbitrary python functions based on trajectory
-    # TODO: correction term
-    # TODO: make grp optional
-
-    def eval(self, grps_eps, return_eps, kappa, l_episodes=100,
-             initial_runs=50):  # TODO: support absolute and relative epsilon
-        start = time.time()
-        ch_bound = st.CH(kappa, grps_eps)  # chernoff-hoeffding bound, number of required samples
-
-        made_runs = 0
+        # compute the CH bound
+        ch_bound = CH(kappa, epsilon)
+        # run the policy until all properties have converged
         while True:
-            logging.debug("starting the next batch of simulations, performed totally %d" % made_runs)
-            self._simulate_runs(initial_runs, l_episodes=l_episodes)
-            made_runs += initial_runs
-            grps_variance = st.binomial_variance(self.loses + self.tos, self.wins, 0,
-                                                 1)  # variance of the binomial distribution for the ratio between wins and runs in general
-            # helps determine the accuracy and confidence of the estimated proportion of wins
-            apmc_bound = st.APMC(grps_variance, kappa,
-                                 grps_eps)  # required sample size to ensure that estimated mean is within accuracy grps_eps with confidence kappa
-            # treat binomial as normal distribution (CLT)
+            # run the policy for the specified number of episodes
+            self.run_policy(agent, self.episodes_per_run, results_per_property)
+            made_episodes += self.episodes_per_run
 
-            interval_length_r = st.construct_confidence_interval_length(self.returns,
-                                                                        kappa)  # length of a confidence interval for the mean of the return
-            interval_length_grp = st.construct_binomial_confidence_interval_length(self.loses + self.tos, self.wins,
-                                                                                   kappa=kappa)  # length of a confidence interval for the ratio
+            # compute for each property the apmc bound and the confidence interval length
+            for property in self.properties.values():
+                variance = get_variance(results_per_property[property.name], epsilon, kappa)
+                apmc_bound = APMC(get_variance(variance, kappa, epsilon))
+                confidence_interval_length = construct_confidence_interval_length(results_per_property[property.name], epsilon, kappa)
 
-            return_criterion = interval_length_r < 2 * return_eps  # one side of the interval has to be smaller than our accuracy
-            grps_criterion = (
-                    (made_runs > apmc_bound)  # these bounds already tell us the maximum number of necessary episodes
-                    or (made_runs > ch_bound)
-                    or (interval_length_grp < 2 * grps_eps)  # see above
-            )
-            print(self.results())  # TODO: Improve the Output (save to File)
-            if (return_criterion and grps_criterion):
+                # check if the property has converged
+                if made_episodes > ch_bound or made_episodes > apmc_bound or confidence_interval_length < 2 * epsilon:
+                    converged_per_property[property.name] = True
+            
+            if all(converged_per_property.values()):
                 break
 
-        end = time.time()
-        print("Finished evaluation of agent in %.2f sec using %d eps" % ((end - start), self._sim_len()))
-
-    def results(self, exhaustive=False):
-        if exhaustive:
-            return self.returns[-1], self.returns[-2], self.returns[-3], self.returns[-4]
-        else:
-            return np.mean(self.returns), self.wins / (self.wins + self.loses + self.tos)
