38834c72354382a5e2dc6cd53f4156baf8cd126e
Nicola J. Müller <106013851+nicola-mueller@users.noreply.github.com>
2024-09-02 16:51:08 +0200

    re-implemented core structure of Evaluator

--- dsmc/evaluator.py
+++ dsmc/evaluator.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from typing import List, Tuple, Any, Dict
+
 
 def CH(kappa, eps):
     x = 1 / np.power(eps, 2)
@@ -13,30 +15,30 @@ def APMC(s2, kappa, eps):
 
     return np.ceil(4 * z * s2 / np.power(eps, 2))
 
-def get_variance(results, epsilon, kappa):
+def get_variance(results, kappa, epsilon):
     pass
 
-def construct_confidence_interval_length(results, epsilon, kappa):
+def construct_confidence_interval_length(results, kappa, epsilon):
     pass
 
-
+# TODO: ALWAYS USE TYPE HINTS!!!
 # base class for evaluation properties
 class Property:
-        def __init__(self, name):
-            self.name = name
-            pass
+    def __init__(self, name: str):
+        self.name = name
+        pass
 
-        # we assume a trajectory is a list of tuples (state, action, reward)
-        def check(self, trajectory: list) -> float:
-            pass
+    # we assume a trajectory is a list of tuples (observation, action, reward)
+    def check(self, trajectory: List[Tuple[Any, Any, Any]]) -> float:
+        pass
 
 # metric that checks if the goal has been reached
 class GoalReachingProbabilityProperty(Property):
-    def __init__(self, name, goal_reward):
+    def __init__(self, name: str, goal_reward: float):
         super().__init__(name)
         self.goal_reward = goal_reward
 
-    def check(self, trajectory):
+    def check(self, trajectory: List[Tuple[Any, Any, Any]]) -> float:
         if trajectory[-1][2] == self.goal_reward:
             return 1.0
         else:
@@ -44,11 +46,11 @@ class GoalReachingProbabilityProperty(Property):
 
 # metric that calculates the return of a trajectory
 class ReturnProperty(Property):
-    def __init__(self, name, gamma):
+    def __init__(self, name: str, gamma: float):
         super().__init__(name)
         self.gamma = gamma
 
-    def check(self, trajectory):
+    def check(self, trajectory: List[Tuple[Any, Any, Any]]) -> float:
         ret = 0
         for t in range(len(trajectory)):
             ret += trajectory[t][2] * np.power(self.gamma, t)
@@ -56,18 +58,20 @@ class ReturnProperty(Property):
 
 class Evaluator:
 
-    def __init__(self, env: GymEnv, gamma: float, initial_episodes: int, episodes_per_run: int, ):
+    def __init__(self, env: GymEnv, gamma: float, initial_episodes: int, episodes_per_run: int):
         self.env = env
         self.gamma = gamma
         self.initial_episodes = initial_episodes
         self.episodes_per_run = episodes_per_run
+
         self.properties = {}
 
     # register a new evaluation property
-    def register_metric(self, property: Property):
+    def register_property(self, property: Property):
         self.properties[property.name] = property
 
-    def run_policy(self, agent, num_episodes, results_per_property):
+    def run_policy(self, agent, num_episodes: int, results_per_property: Dict[str, EvaluationResults]):
+        # TODO: this is only a barebones RL loop
         for _ in range(num_episodes):
             state = self.env.reset()
             trajectory = []
@@ -78,10 +82,12 @@ class Evaluator:
                 trajectory.append((state, action, reward))
                 state = next_state
 
+            # store new results in EvaluationResults object
             for property in self.properties.values():
-                results_per_property[property.name].append(property.check(trajectory))
+                # TODO: EvaluationResults class needs some kind of extend function that adds new results
+                results_per_property[property.name].extend(property.check(trajectory))
 
-    def eval(self, agent, epsilon, kappa):
+    def eval(self, agent, epsilon: float, kappa: float):
         # initialize EvaluationResults object for each class and whether the property converged
         results_per_property = {}
         converged_per_property = {}
@@ -101,16 +107,20 @@ class Evaluator:
             self.run_policy(agent, self.episodes_per_run, results_per_property)
             made_episodes += self.episodes_per_run
 
-            # compute for each property the apmc bound and the confidence interval length
+            # compute for each property the APMC bound and the confidence interval length
             for property in self.properties.values():
-                variance = get_variance(results_per_property[property.name], epsilon, kappa)
-                apmc_bound = APMC(get_variance(variance, kappa, epsilon))
-                confidence_interval_length = construct_confidence_interval_length(results_per_property[property.name], epsilon, kappa)
+                property_results = results_per_property[property.name]
 
-                # check if the property has converged
+                apmc_bound = APMC(property_results.get_variance(), kappa, epsilon)
+                confidence_interval_length = construct_confidence_interval_length(property_results, kappa, epsilon)
+
+                # check if the property has converged, property can also become non-converged again!!!
                 if made_episodes > ch_bound or made_episodes > apmc_bound or confidence_interval_length < 2 * epsilon:
                     converged_per_property[property.name] = True
-            
+                else:
+                    converged_per_property[property.name] = False
+
             if all(converged_per_property.values()):
                 break
 
+        return results_per_property
